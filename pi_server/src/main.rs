210987071d9236e76551aa78be54924c2b03fe3e
Lucas Keller <kellerjlucas@gmail.com>
2023-07-02 09:20:56 -0500

    win_client and pi_server setup
    Have two seperate bins in this workspace: win_client and pi_server.
    
    Right now, both communicate over home:7878.
    
    I need to get the client/server off of home and query util data.

--- pi_server/src/main.rs
+++ pi_server/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, from the server!");
+use std::io;
+use std::time;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+const MAX_STREAM_READS: usize = 60;
+
+fn handle_sender(mut stream: TcpStream) -> io::Result<()> {
+    let mut buf = [0; 512];
+    for _ in 0..MAX_STREAM_READS {
+        let bytes_read = stream.read(&mut buf)?;
+
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write(&buf[..bytes_read])?;
+
+        println!("from the sender: {}", String::from_utf8_lossy(&buf));
+        
+        // reduce overhead of looking for more client data
+        thread::sleep(time::Duration::from_secs(1));
+    }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    println!("Pi Server is running...");
+
+    let receiver_listener = TcpListener::bind("127.0.0.1:7878").expect("Failed bind with sender");
+
+    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+
+    for stream in receiver_listener.incoming() {
+        let stream = stream.expect("Failed to get stream from receiver_listener");
+        
+        // let receiver connect with sender
+        let handle = thread::spawn(move || {
+            handle_sender(stream).unwrap_or_else(|error| eprintln!("{:?}", error))
+        });
+
+        thread_vec.push(handle);
+    }
+
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+
+    Ok(())
 }
--- win_client/src/main.rs
+++ win_client/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, from the client!");
+use std::str;
+use std::net::TcpStream;
+use std::io::{self, prelude::*, BufReader, Write};
+
+const MAX_STREAM_WRITES: usize = 60;
+
+fn main() -> io::Result<()> {
+    println!("Win Client is running...");
+
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
+
+    for _ in 0..MAX_STREAM_WRITES {
+        let mut input = String::new();
+        
+        println!("Enter a message:");
+        io::stdin().read_line(&mut input).expect("Failed to read");
+
+        stream.write(input.as_bytes()).expect("Failed to write");
+
+        let mut reader = BufReader::new(&stream);
+        let mut buffer: Vec<u8> = Vec::new();
+        reader.read_until(b'\n', &mut buffer)?;
+
+        println!("read from server: {} \n", str::from_utf8(&buffer).unwrap());
+    }
+
+    Ok(())
 }
